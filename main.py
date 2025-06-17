9746f7e26f133e80ce8de51e6a441fbf8571b099
retrogramx <aalvarez0295@gmail.com>
2025-06-17 15:39:35 -0700

    added covert_words_to_num function

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ def get_book_text(filepath):
 		file_contents = f.read()
 	return file_contents
 
+def convert_words_to_num(file_contents):
+	words = file_contents.split()
+	num_words = len(words)
+	return num_words
+
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    num_words = convert_words_to_num(book_text)
+    print(f"{num_words} words found in the document")
 main()
