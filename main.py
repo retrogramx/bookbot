59ee32132e716820232169d6f7b2852bc76582c6
retrogramx <aalvarez0295@gmail.com>
2025-06-18 13:44:32 -0700

    split files added import stats

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
+from stats import convert_words_to_num
+
 def get_book_text(filepath):
 	with open(filepath) as f:
 		file_contents = f.read()
 	return file_contents
-
-def convert_words_to_num(file_contents):
-	words = file_contents.split()
-	num_words = len(words)
-	return num_words
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,4 @@
+def convert_words_to_num(file_contents):
+	words = file_contents.split()
+	num_words = len(words)
+	return num_words
