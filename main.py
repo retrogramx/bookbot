f6d7fbc22ff6de8ced5a8608494d5f274be69b62
retrogramx <aalvarez0295@gmail.com>
2025-06-18 21:20:40 -0700

    added count_characters function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import convert_words_to_num
+from stats import convert_words_to_num, count_characters
 
 def get_book_text(filepath):
 	with open(filepath) as f:
@@ -9,4 +9,6 @@ def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = convert_words_to_num(book_text)
     print(f"{num_words} words found in the document")
+    characters = count_characters(book_text)
+    print(f"{characters}")
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,14 @@ def convert_words_to_num(file_contents):
 	words = file_contents.split()
 	num_words = len(words)
 	return num_words
+
+def count_characters(file_contents):
+	characters = {}
+
+	for character in file_contents:
+		character = character.lower()
+		if character in characters:
+			characters[character] = characters[character] + 1
+		else:
+			characters[character] = 1
+	return characters
