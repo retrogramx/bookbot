76610fe957dbcee9db83f5170f01103e5937145e
retrogramx <aalvarez0295@gmail.com>
2025-06-19 11:47:20 -0700

    added sorting by numbers function and clean reporting

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import convert_words_to_num, count_characters
+from stats import convert_words_to_num, count_characters, sort_dict
 
 def get_book_text(filepath):
 	with open(filepath) as f:
@@ -6,9 +6,24 @@ def get_book_text(filepath):
 	return file_contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    num_words = convert_words_to_num(book_text)
-    print(f"{num_words} words found in the document")
-    characters = count_characters(book_text)
-    print(f"{characters}")
+	print(f'============ BOOKBOT ============')
+	book_path = 'books/frankenstein.txt'
+	book_text = get_book_text(book_path)
+	print(f'Analyzing book found at {book_path}...')
+	print(f'----------- Word Count ----------')
+	num_words = convert_words_to_num(book_text)
+	print(f'Found {num_words} total words')
+	print(f'--------- Character Count -------')
+	characters = count_characters(book_text)
+
+	list_of_dicts = sort_dict(characters)
+	
+	for dict_item in list_of_dicts:
+		char = dict_item['char']
+		num = dict_item['num']
+		if char.isalpha():		    
+			print(f"{char}: {num}")
+	print(f'============= END ===============')
+
+
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,12 @@ def count_characters(file_contents):
 		else:
 			characters[character] = 1
 	return characters
+
+def sort_on(list_of_dicts):
+	return list_of_dicts['num']
+
+def sort_dict(characters):
+	# Convert dictionary to a list of dictionaries
+	list_of_dicts = [{'char': key, 'num': value} for key, value in characters.items()]
+	list_of_dicts.sort(reverse=True, key=sort_on)
+	return list_of_dicts
